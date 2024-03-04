7327bc8b55e1cf44a32de0faab60f800f836b674
Vartika Rungta <scvr@leeds.ac.uk>
2024-03-04 15:14:31 +0000

    1

--- tests/hypotenuse.py
+++ tests/hypotenuse.py
@@ -5,25 +5,25 @@ import functions.hp_calc.py as hc
 
 
 def hyp1():
-    assert fc.hyp(-3, -4) > 0, "two negative should return a positive"
+    assert hc.hyp(-3, -4) > 0, "two negative should return a positive"
 
 
 def hyp2():
-    assert fc.hyp(3, -9) > 0, "one +ve an done -ve returns a positive"
+    assert hc.hyp(3, -9) > 0, "one +ve an done -ve returns a positive"
 
 
 def hyp3():
-    assert fc.hyp(0, 0) == 0, "2 zeroes returns a zero"
+    assert hc.hyp(0, 0) == 0, "2 zeroes returns a zero"
 
 
 def hyp4():
-    assert fc.hyp(7, 2.5) == pytest.approx(
+    assert hc.hyp(7, 2.5) == pytest.approx(
         7.43, 0.01
     ), "square roots of 2 floats returns a float with their added precision"
 
 
 def hyp4():
-    assert fc.hyp(np.infty, 0) == np.infty, "square root if infinity is infinity"
+    assert hc.hyp(np.infty, 0) == np.infty, "square root if infinity is infinity"
 
 
 # def hyp5():
